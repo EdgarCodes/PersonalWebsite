72d0c91306797fd7bdc08967cdbcf4589f039b16
Edgar Gonzalez <Edggar@Gmail.com>
2020-07-21 14:32:41 -0400

    Updates

---
+++ precache-manifest.a9f6d46683fd7f67ac20a632964b25fd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961b8b1eb8d327f205457e73f1459f76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8259816881a8d98df98b",
+    "url": "/static/css/main.6a2b9c35.chunk.css"
+  },
+  {
+    "revision": "0869976398371efd0263",
+    "url": "/static/js/2.e006d6e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e006d6e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8259816881a8d98df98b",
+    "url": "/static/js/main.b89f9783.chunk.js"
+  },
+  {
+    "revision": "9734eb3cba0fa9d5ef84",
+    "url": "/static/js/runtime-main.64fa3b4a.js"
+  },
+  {
+    "revision": "87014735db479bfb4a261b69cf4d3e5e",
+    "url": "/static/media/159503493365.87014735.png"
+  },
+  {
+    "revision": "e1e73f654d0f397a74f85d04d6aacdb2",
+    "url": "/static/media/Basic_Logo.e1e73f65.png"
+  },
+  {
+    "revision": "331dbd04dab86203f82fb7bc28f5987b",
+    "url": "/static/media/Blog_1.331dbd04.jpg"
+  },
+  {
+    "revision": "07ea9cb965a4195bdf2eec9986edc136",
+    "url": "/static/media/Blog_1_preview.07ea9cb9.jpg"
+  },
+  {
+    "revision": "f8cc317dbb3f91aa13846770457cf3c6",
+    "url": "/static/media/Blog_2.f8cc317d.jpg"
+  },
+  {
+    "revision": "15cf4d6b39ebf4146f6bec611af23ea2",
+    "url": "/static/media/Blog_2_preview.15cf4d6b.jpg"
+  },
+  {
+    "revision": "82275cebf477dea2cdbf0b869c7e25e5",
+    "url": "/static/media/Graduation_Picture.82275ceb.jpg"
+  },
+  {
+    "revision": "3c4b700087aa4a05f89caadb11abe82c",
+    "url": "/static/media/NHS_Member_Badge-300x300.3c4b7000.png"
+  },
+  {
+    "revision": "39bb1e8803c56e829de35e21530826a5",
+    "url": "/static/media/Resume.39bb1e88.pdf"
+  },
+  {
+    "revision": "ace78f5d21a71139925bc1c225e176d6",
+    "url": "/static/media/blog-background.ace78f5d.jpg"
+  },
+  {
+    "revision": "6cce95e7b3e52b4a3b7462c493e1ac0c",
+    "url": "/static/media/blog_3_preview.6cce95e7.jpg"
+  },
+  {
+    "revision": "47a32abbcbfeee96ef0927437aaa22d6",
+    "url": "/static/media/homepage-background-2.47a32abb.jpg"
+  },
+  {
+    "revision": "06dd35a4b860fcacd9a69f21694b3945",
+    "url": "/static/media/project-pokedex-preview.06dd35a4.jpg"
+  },
+  {
+    "revision": "854ccf17ca1e62b3ce8fb137cf3dd071",
+    "url": "/static/media/project-preview-image.854ccf17.jpg"
+  },
+  {
+    "revision": "4dc8d134e57ec4e8698ed44d772332a8",
+    "url": "/static/media/project-schedule-preview.4dc8d134.jpg"
+  },
+  {
+    "revision": "9ce4f8cb5047d34f960e0c140593c654",
+    "url": "/static/media/projects-background.9ce4f8cb.jpg"
+  }
+]);
